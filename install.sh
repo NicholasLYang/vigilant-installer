6f08b62004908cef7c65a9bb30a07e3453730da7
Nicholas Yang <nick@nicholasyang.com>
2016-06-03 13:19:01 -0400

    added to installer

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
+mkdir /usr/www
 sudo apt-get update
 sudo apt-get install Imagemagick python-pip sqlite emacs git
-git clone https://github.com/daisyb/vigilant-web-gallery.git
+echo "What is your domain? I.e. www.YOUR_DOMAIN_HERE.com"
+read domain
+git clone https://github.com/daisyb/vigilant-web-gallery.git /usr/www/www.$domain.com
