41b3388ce82a46f830ad39a0255358ce0852b48d
Nicholas Yang <nick@nicholasyang.com>
2016-06-03 19:28:37 -0400

    fixes

--- install_local.sh
+++ install_local.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
-echo "Please give server IP"
-read ip
 
+ssh grimes "apt-get install git; git clone https://github.com/NicholasLYang/vigilant-installer.git; cd vigilant-installer; . install_server1.sh"
 
-
-ssh root@$ip "apt-get install git; git clone https://github.com/NicholasLYang/vigilant-installer.git; cd vigilant-installer; . install_server_1.sh"
 
 
 
--- install_server1.sh
+++ install_server1.sh
@@ -3,14 +3,16 @@
 mkdir /var/www
 apt-get update
 apt-get install Imagemagick python-pip sqlite emacs git
-echo "What is your domain? I.e. www.YOUR_DOMAIN_HERE.com"
+echo "What is your domain? "
 read domain
-git clone https://github.com/daisyb/vigilant-web-gallery.git /usr/www/www.$domain.com
+git clone https://github.com/daisyb/vigilant-web-gallery.git /usr/www/$domain
+echo "Are you the master of your domain?"
 cd /var
 mkdir repo && cd repo
 mkdir site.git && cd site.git
 git init --bare
 cd hooks
+touch post-receive
 echo "#!/bin/sh" post-receive
 echo "git --work-tree=/var/www/domain.com --git-dir=/var/repo/site.git checkout -f" post-receive
 chmod +x post-receive
