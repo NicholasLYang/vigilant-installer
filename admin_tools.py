9dd067e9eaaa706bbf0cfc8ba0c6ef51f49ed08d
Daisy <dbarbanel555@gmail.com>
2016-06-06 19:54:32 -0400

    admin tools basically done, just need years api for archiving

--- admin_tools.py
+++ admin_tools.py
@@ -1,8 +1,11 @@
 import urllib2, json
+from sys import exit
+
 ip = raw_input("Please enter the ip address ")
 key = "nyang"
 
 def start():
+    print "If at any time you wish to leave, type 'exit'"
     print "\nWelcome Mr. DW!"
     print "What would you like to do today?\n"
     main()
@@ -12,10 +15,12 @@ def main():
     functions = [
         "Delete Image",
         "List Galleries",
-        "Add Gallery",
+        "Create Gallery",
         "Delete Gallery",
         "List Years",
-        "Archive Year"]
+        "Archive Year",
+        "Unarchive Year"
+    ]
     
     print "Choose the number of the option you'd like to select:"
     goal = userInput(functions)
@@ -29,11 +34,11 @@ def main():
         confirm = raw_input("\nAre you sure want to delete %s in %s? (y/n) "%(image,gallery))
 
         if isY(confirm):
+            print #empty line
             print "Deleting . . ."
             print deleteImage(image, gallery)
         else:
             print "Deletion canceled"
-        print #empty line
 
     elif goal == 1: #list galleries
         galleries = getGalleries()
@@ -43,8 +48,8 @@ def main():
     elif goal == 2: #add gallery #work in progress
         print "\nWhat would you like to name your new gallery?"
         name = raw_input()
-        response = createGallery(name)
-        print response
+        print createGallery(name)
+        
     elif goal == 3: #delete gallery
         print "\nWhich gallery would you like to delete?"
         gallery = askGallery()
@@ -52,11 +57,11 @@ def main():
         confirm = raw_input()
 
         if isY(confirm):
+            print #empty line
             print "Deleting . . ."
             print deleteGallery(gallery)
         else:
             print "Deletion canceled"
-        print #empty line        
         
     elif goal == 4: #list years #work in progress
         """
@@ -66,18 +71,18 @@ def main():
         """
         print "Feature coming soon"
         
-    elif goal == 5: #archive year #work in progress
+    elif goal == 5: #archive year 
         print "Which year would you like to archive?" 
-        year = raw_input() 
-        print "Are you sure you want to archive the galleries from (%s)? (y/n)"%(year)
-        confirm = raw_input()
-
-        if isY(confirm):
-            return archiveYear(year)
-        else:
-            print "Archive canceled"
+        year = raw_input()
+        print archiveGalleries(year)
+        
+    elif goal == 6: #unarchives year
+        print "Which year would you like to unarchive?"
+        year = raw_input()
+        print unarchiveGalleries(year)
         
     #Asks at the end if user would like to continue or exit
+    print #empty line
     print "Continue? (y/n) "
     confirm = raw_input()
     if isY(confirm):
@@ -89,55 +94,77 @@ def printList(coll):
     for index, item in enumerate(coll):
         print str(index) + " " + item
     print
-   
-def callAPI(url):
+
+#sendingData is supposed to determine whether the
+#api is sending json data or not
+def callAPI(url,sendingData):
    # print url
     request = urllib2.urlopen(url)
     result = request.read()
-    return  json.loads(result)
+    if result == "Error":
+        print "Error, invalid key"
+        exit()
+    if (sendingData):
+        return  json.loads(result)
+    return result
 
 def getImages(gallery):
     uri = "http://" + ip + "/getimagename/%s/%s"
     url = uri%(key, gallery)
-    return callAPI(url)
+    return callAPI(url, True)
 
 def getGalleries():
     uri = "http://" + ip + "/getgalleries/%s"
     url = uri%(key)
-    return callAPI(url)
+    return callAPI(url, True)
     
 def deleteImage(img, gallery):
     uri = "http://" + ip + "/deleteimage/%s/%s/%s"
     url = uri%(key, gallery, img)
-    out = callAPI(url)
-    if out == "Error":
-        return out
-    return "%s in %s has been deleted"%(img, gallery)
+    out = callAPI(url, False)
+    if out == "success":
+        return "%s in %s has been deleted"%(img, gallery)
+    print out
+    return "Error, image not deleted"
 
 def deleteGallery(gallery):
     uri = "http://" + ip + "/deletegallery/%s/%s"
     url = uri%(key, gallery)
-    out = callAPI(url)
-    if out == "Error":
-        return out
-    return gallery + " has been deleted"
+    out = callAPI(url, False)
+    if out == "success":
+        return gallery + " has been deleted"
+    print out
+    return "Error, gallery not deleted"
 
 def createGallery(gallery):
     uri = "http://" + ip + "/creategallery/%s/%s"
     url = uri%(key, gallery)
-    out = callAPI(url)
-    if out == "Error":
-        return out
-    return "The" + gallery + " has been created"
+    out = callAPI(url, False)
+    if out == "success":
+        return "\n" + gallery + " has been created"
+    print out
+    return "Error, gallery not created"
 
 def archiveGalleries(year):
     uri = "http://" + ip + "/archivegalleries/%s/%s"
     url = uri%(key, year) 
-    out = callAPI(url)
-    if out == "Error":
-        return out
-    return "The galleries from " + year +" have been archived" 
+    out = callAPI(url, False)
+    print out
+    if out == "success":
+        return "\nThe galleries from " + year +" have been archived"
+    print out
+    return "Error, galleries not archived"
 
+def unarchiveGalleries(year):
+    uri = "http://" + ip + "/unarchivegalleries/%s/%s"
+    url = uri%(key, year) 
+    out = callAPI(url, False)
+    print out
+    if out == "success":
+        return "\nThe galleries from " + year +" have been unarchived"
+    print out
+    return "Error, galleries not unarchived"
+    
 def askGallery():
     galleries = getGalleries()
     galNum = userInput(galleries)
@@ -160,6 +187,8 @@ def strRange(num):
 def userInput(options):
     printList(options)
     inpt = raw_input()
+    if inpt == "exit":
+        exit()
     validVals = strRange(len(options))
     while( inpt not in validVals ):
         print "\nI'm sorry, DW. I'm afraid I can't do that."
